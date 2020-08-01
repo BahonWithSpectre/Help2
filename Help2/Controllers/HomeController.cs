518729fa428850071377c8bf9ab9c0433f3fc880
bakytzhan.shymkentay <bakytzhan.shymkentay@DESKTOP-RM08NSD>
2020-08-01 13:04:49 +0300

    Добавьте файлы проекта.

---
+++ Help2/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Help2.Models;
+using Microsoft.Extensions.Logging;
+using Help2.DbFolder;
+using Help2.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Help2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDb db;
+
+        public HomeController(ILogger<HomeController> logger, AppDb _db)
+        {
+            _logger = logger;
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            var list = db.Helps.Include(p => p.City).ToList();
+
+            ViewBag.City = db.Cities.ToList();
+
+            return View(list);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string category, int? city, string type)
+        {
+            var list = db.Helps.Include(p => p.City).ToList();
+
+            ViewBag.City = db.Cities.ToList();
+
+            //if (category.Length != 0)
+            //{
+            //    list = list.Where(p => p.Category == category).ToList();
+            //}
+            if (city != null && city != 0)
+            {
+                list = list.Where(p => p.CityId == city).ToList();
+            }
+            if (type != null && type != "all")
+            {
+                list = list.Where(p => p.Type == type).ToList();
+            }
+
+            return View(list);
+        }
+
+
+
+        public IActionResult Creat()
+        {
+            ViewBag.City = db.Cities.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Creat(Help help)
+        {
+            var tt = help;
+
+            db.Helps.Add(help);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Help2/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Help2.DbFolder;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Help2.Data
+{
+    public class AppDb : IdentityDbContext<User>
+    {
+        public AppDb(DbContextOptions<AppDb> options)
+            : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Help> Helps { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
